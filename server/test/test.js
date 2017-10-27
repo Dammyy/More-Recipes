b32247b9d5ca5db22f417b8271e8047ce955cb5e
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-10-27 16:47:40 +0100

    add test file for more-recipes project

---
+++ server/test/test.js
@@ -0,0 +1,31 @@
+import chaiHttp from 'chai-http';
+import chai from 'chai';
+import app from '../../app';
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+describe('Test API', () => {
+  describe('GET /', () => {
+    // Test for index route
+    it('Should return 200', (done) => {
+      chai.request(app)
+        .get('/')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          done();
+        });
+    });
+
+    it('Undefined Routes Should Return 404', (done) => {
+      chai.request(app)
+        .post('/another/undefined/route')
+        .send({ random: 'random' })
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    }); 
+  });
+});
