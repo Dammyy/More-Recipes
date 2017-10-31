c62932d4c0533415981667f364e4e8617cd403d9
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-10-31 19:16:22 +0100

    update app.js file

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import expressJWT from 'express-jwt';
 import routes from './server/routes/index';
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressJWT({ secret: 'Damilare Andela' }).unless({ path: ['/api/v1/users/signin', '/api/v1/recipes'] }));
 routes(app);
 
 app.get('/', (req, res) => res.status(200).send({
