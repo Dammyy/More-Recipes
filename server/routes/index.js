3247436ad0b6255a6fd1819478aeca22878b9cff
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-11-02 20:19:34 +0100

    update routes file index.js to include new routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,12 +5,14 @@ import users from '../controller/user';
 import auth from '../middlewares/auth';
 
 export default (app) => {
-  // app.post('/api/v1/recipes/:recipeId/reviews', Recipes.addReview);
+  app.post('/api/v1/recipes/:recipeId/reviews', auth, Recipes.postReview);
   app.post('/api/v1/recipes', auth, Recipes.createRecipes);
   app.get('/api/v1/recipes', Recipes.getRecipe);
   app.put('/api/v1/recipes/:recipeId', auth, Recipes.updateRecipes);
   app.delete('/api/v1/recipes/:recipeId', auth, Recipes.deleteRecipes);
   app.get('/api/v1/recipes/:recipeId', Recipes.retrieveRecipes);
+  app.get('/api/v1/users/:userId/recipes', auth, users.getFavorites);
+  app.post('/api/v1/recipes/:recipeId/favorites', auth, Recipes.favoriteRecipe);
   app.post('/api/v1/users/signup', users.signUp);
   app.post('/api/v1/users/signin', users.signIn);
 };
