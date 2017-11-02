69d36ccc1b944ac58069d3a5cfed8da88d7318c4
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-11-02 13:05:46 +0100

    update user models file

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('user', {
+  const User = sequelize.define('users', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
     hashPassword: DataTypes.STRING
   });
+  User.associate = (models) => {
+    User.hasMany(models.recipes, {
+      foreignKey: 'userId',
+      as: 'recipes',
+    });
+  };
   return User;
 };
