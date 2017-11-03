5b5e27ce1c99fef96cf494831afbf0a22414c9f9
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-11-03 08:16:10 +0100

    upage config.js file

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,7 @@ module.exports = {
     dialect: 'postgres',
   },
   production: {
-    username: 'msuerivuccucve',
-    password: '8212682f93a6ee5c73e571578b090fb12884c902c061ed922832670d2fc8452e',
-    database: '',
-    host: 'ec2-54-235-150-134.compute-1.amazonaws.com:5432/d9k55gp0fgg32q',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
   },
 };
