fa15fb78603bd1d082691a10d549d0eee5de09b1
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-12-01 20:07:21 +0100

    add react components jsx files

---
+++ client/src/components/index.js
@@ -0,0 +1,11 @@
+import Home from './Home.jsx';
+import Welcome from './Welcome.jsx';
+import Mrecipes from './Manage.jsx';
+import Archive from './Archive.jsx';
+import Modal from './Modal.jsx';
+import Form from './Form.jsx';
+import Recipe from './Recipe.jsx';
+import RecipesListManager from './RecipesListManager.jsx';
+// We export all the components at once
+export { Home, Welcome, Mrecipes, Form, Recipe,
+  RecipesListManager, Archive, Modal };
