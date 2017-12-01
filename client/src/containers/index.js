57a69d5775aba61d2e340f9c4a13a9ec49daf9d5
Damilare Olatubosun <damilareolatubosun@gmail.com>
2017-12-01 20:08:20 +0100

    add react container files

---
+++ client/src/containers/index.js
@@ -0,0 +1,8 @@
+import AddRecipeContainer from './AddRecipeContainer';
+import RecipesContainer from './RecipesContainer';
+
+// We export all the containers at once
+export {
+  AddRecipeContainer,
+  RecipesContainer
+};
