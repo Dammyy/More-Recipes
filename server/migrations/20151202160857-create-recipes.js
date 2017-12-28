510b6427f7f624186b9acf990ad2a153cd8ad704
Damilare Olatubosun <Dami@Andelas-MacBook-Pro-3.local>
2017-12-28 16:24:06 +0100

    add image attribute to recipe model

--- server/migrations/20151202160857-create-recipes.js
+++ server/migrations/20151202160857-create-recipes.js
@@ -16,6 +16,9 @@ module.exports = {
       details: {
         type: Sequelize.TEXT
       },
+      image: {
+        type: Sequelize.TEXT
+      },
       reviews: {
         type: Sequelize.INTEGER
       },
@@ -49,3 +52,4 @@ module.exports = {
     return queryInterface.dropTable('recipes');
   }
 };
+
