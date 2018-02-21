89ddc6c17355cf971a10b1b8ee569e7d407496fc
“Damilare <damilareolatubosun@gmail.com>
2018-02-21 20:20:50 +0100

    chore (fix-eslint): fix eslint rules violations
    [#155048249]

--- client/src/containers/AddRecipe.jsx
+++ client/src/containers/AddRecipe.jsx
@@ -18,17 +18,11 @@ export default class AddRecipe extends Component {
    */
   constructor(props) {
     super(props);
-<<<<<<< HEAD:client/src/containers/AddRecipe.jsx
     this.state = { newRecipe: {} };
-=======
-    this.state = { newRecipe: {}};
->>>>>>> b011d55da38b00d516147b716f5608f74950885c:client/src/containers/AddRecipeContainer.jsx
     this.submit = this.submit.bind(this);
     this.uploadPicture = this.uploadPicture.bind(this);
     this.setRecipe = this.setRecipe.bind(this);
   }
-<<<<<<< HEAD:client/src/containers/AddRecipe.jsx
-
 
   /**
    *
@@ -53,9 +47,6 @@ export default class AddRecipe extends Component {
    * @memberOf AddRecipe
    */
   submit() {
-=======
-  submit () {
->>>>>>> b011d55da38b00d516147b716f5608f74950885c:client/src/containers/AddRecipeContainer.jsx
     const newRecipe = Object.assign({}, { picture: $('#picture').attr('src') }, this.state.newRecipe);
     fetch('http://localhost:3000/api/v1/recipes', {
       headers: new Headers({
@@ -64,19 +55,11 @@ export default class AddRecipe extends Component {
       method: 'POST',
       body: JSON.stringify(newRecipe)
     })
-<<<<<<< HEAD:client/src/containers/AddRecipe.jsx
       .then(response => response.json())
       .then((data) => {
         console.log(data.message);
         hashHistory.push('/catalog');
       });
-=======
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.message);
-      hashHistory.push('/catalog');
-    });
->>>>>>> b011d55da38b00d516147b716f5608f74950885c:client/src/containers/AddRecipeContainer.jsx
   }
 
   /**
@@ -102,7 +85,6 @@ export default class AddRecipe extends Component {
       }
     );
   }
-<<<<<<< HEAD:client/src/containers/AddRecipe.jsx
   /**
    *
    *
@@ -116,18 +98,5 @@ export default class AddRecipe extends Component {
       uploadPicture={this.uploadPicture}
       setRecipe={this.setRecipe}
     />);
-=======
-  setRecipe () {
-    const newRecipe = {
-      title: document.getElementById('title').value,
-      ingredients: document.getElementById('ingredients').value,
-      details: document.getElementById('details').value,
-      image: $('#picture').attr('src')
-    };
-    this.setState({ newRecipe });
-  }
-  render () {
-    return <Form submit={this.submit} uploadPicture={this.uploadPicture} setRecipe={this.setRecipe} />
->>>>>>> b011d55da38b00d516147b716f5608f74950885c:client/src/containers/AddRecipeContainer.jsx
   }
 }
